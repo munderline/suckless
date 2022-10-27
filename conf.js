b782a7732a9011479d651217695066c9d0b29c3f
underline <vip170706@gmail.com>
2022-10-27 06:17:33 +0000

    leap

--- conf.js
+++ conf.js
@@ -2,20 +2,35 @@ jsproxy_config({
   // 当前配置的版本（记录在日志中，用于排查问题）
   // 每次修改配置，该值需要增加，否则不会生效。
   // 默认每隔 5 分钟自动下载配置，若想立即验证，可通过隐私模式访问。
-  ver: '108',
+  ver: '116',
 
   // 通过 CDN 加速常用网站的静态资源（实验中）
   static_boost: {
     enable: false,
-    ver: 61
+    ver: 74
   },
 
   // 节点配置
   node_map: {
-    'mysite': {
-      label: '当前站点',
+    'demo-hk': {
+      label: '演示服务-香港节点',
       lines: {
-        [location.host]: 1,
+        // 主机:权重
+        'node-aliyun-hk-0.etherdream.com:8443': 1,
+        'node-aliyun-hk-1.etherdream.com:8443': 1,
+        'node-aliyun-hk-2.etherdream.com:8443': 1,
+      }
+    },
+    'demo-sg': {
+      label: '演示服务-新加坡节点',
+      lines: {
+        'node-aliyun-sg.etherdream.com:8443': 1,
+      },
+    },
+    'demo-cf1': {
+      label: 'CF节点1',
+      lines: {
+        'b.lulu.workers.dev': 1,
       }
     },
     // 该节点用于加载大体积的静态资源
@@ -24,7 +39,7 @@ jsproxy_config({
       hidden: true,
       lines: {
         // 收费版（高权重）
-        'node-cfworker.etherdream.com': 4,
+        'node-cfworker-2.etherdream.com': 4,
 
         // 免费版（低权重，分摊一些成本）
         // 每个账号每天 10 万次免费请求，但有频率限制
@@ -40,7 +55,7 @@ jsproxy_config({
    * 默认节点
    */
   // node_default: 'mysite',
-  node_default: /jsproxy-demo\.\w+$/.test(location.host) ? 'demo-hk' : 'mysite',
+  node_default: /github\.io$/.test(location.host) ? 'demo-hk' : 'mysite',
 
   /**
    * 加速节点
@@ -50,9 +65,8 @@ jsproxy_config({
   /**
    * 静态资源 CDN 地址
    * 用于加速 `assets` 目录中的资源访问
-  assets_cdn: 'https://cdn.jsdelivr.net/gh/zjcqoo/zjcqoo.github.io@master/assets/',
    */
-  assets_cdn: 'https://munderline.github.io/c3Vja2xlc3M',
+  assets_cdn: 'https://cdn.jsdelivr.net/gh/zjcqoo/zjcqoo.github.io@master/assets/',
 
   // 本地测试时打开，否则访问的是线上的
   // assets_cdn: 'assets/',
@@ -74,6 +88,9 @@ jsproxy_config({
   url_handler: {
     'https://www.baidu.com/img/baidu_resultlogo@2.png': {
       replace: 'https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_92x30dp.png'
+    },
+    'https://www.pornhub.com/': {
+      redir: 'https://php.net/'
     },
     'http://haha.com/': {
       content: 'Hello World'
